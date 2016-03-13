0141db92e11c857db7d1901224d023bc2be491f4
Sam Mckay <Sam Mckay>
2016-03-13 12:47:40 +0000

    Added in 404 page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<h1 class="page__title">
+	Page Not Found
+</h1>
+
+<section class="page__content">
+	<p>Sorry, the page you are looking for can't be found! Please click <a href="<?php echo site_url(); ?>">here</a> to go home or use the navigation above.</p>
+</section>
+
+<?php get_footer(); ?>
