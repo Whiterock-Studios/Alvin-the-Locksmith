99ab152e2431bd37c79e65165b4101b79a0a1e92
Sam Mckay <Sam Mckay>
2016-03-13 12:49:35 +0000

    FIX : Page Titles

--- header.php
+++ header.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html <?php language_attributes (); ?>>
 <head>
-    <link href='https://fonts.googleapis.com/css?family=Lato:400,700,700italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700' rel='stylesheet' type='text/css'>
+    <!-- <link href='https://fonts.googleapis.com/css?family=Lato:400,700,700italic,400italic' rel='stylesheet' type='text/css'> -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <meta charset="<?php bloginfo ( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php wp_title ( '|' , true , 'right' ); ?></title>
+    <title><?php
+              echo the_title();
+              echo " | ";
+              echo bloginfo('title');
+            ?>
+    </title>
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo ( 'pingback_url' ); ?>">
     <?php wp_head (); ?>
