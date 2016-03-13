141935582d0aacaafe63f528101ec3bd6c021a3c
Sam Mckay <Sam Mckay>
2016-03-13 14:00:09 +0000

    changed fake flexbox js to work on both home page and services template with the class .equal__box

--- assets/javascripts/_front-page.js
+++ assets/javascripts/_front-page.js
@@ -5,14 +5,14 @@ jQuery(document).ready(function($) {
 			$counter = 0;
 			$height = Array();
 
-			$('.home__section__2__box').each(function() {
+			$('.equal__box').each(function() {
 				$height[$counter] = $(this).outerHeight();
 				$counter++;
 			});
 
 			$biggest = Math.max.apply(Math, $height);
 
-			$('.home__section__2__box').each(function() {
+			$('.equal__box').each(function() {
 				if ($(this).outerHeight() < $biggest){
 					$newSpacer = ($biggest - $(this).outerHeight());
 					$(this).find('.alvin__button').css('margin-top',  $newSpacer + 'px');
